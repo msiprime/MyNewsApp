e3ee224f4492bf3543337c5a9735165dbe17c2f8
msiprime <msisakib958@gmail.com>
2023-11-26 21:14:18 +0600

    done part 6, log showse falls, dagger setup done

---
+++ app/src/main/java/com/msiprime/mynewsapp/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.msiprime.mynewsapp.di
+
+import android.app.Application
+import com.msiprime.mynewsapp.data.manager.LocalUserManagerImpl
+import com.msiprime.mynewsapp.domain.manager.LocalUserManager
+import com.msiprime.mynewsapp.domain.usecase.AppEntryUseCases
+import com.msiprime.mynewsapp.domain.usecase.ReadAppEntry
+import com.msiprime.mynewsapp.domain.usecase.SaveAppEntry
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class) //so they will live as long as the application
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideLocalUserManager(
+        application: Application
+    ): LocalUserManager = LocalUserManagerImpl(application)
+
+    @Provides
+    @Singleton
+    fun provideAppEntryUseCases(
+        localUserManager: LocalUserManager
+    ) = AppEntryUseCases(
+        readAppEntry = ReadAppEntry(localUserManager),
+        saveAppEntry = SaveAppEntry(localUserManager)
+    )
+
+}
---
+++ app/src/main/java/com/msiprime/mynewsapp/domain/usecase/AppEntryUseCases.kt
@@ -0,0 +1,6 @@
+package com.msiprime.mynewsapp.domain.usecase
+
+data class AppEntryUseCases(
+    val readAppEntry: ReadAppEntry,
+    val saveAppEntry: SaveAppEntry
+)
