103f1adfc32f056424f65fc136371fd8c6897347
msiprime <msisakib958@gmail.com>
2023-11-27 14:39:02 +0600

    progress

---
+++ app/src/main/java/com/msiprime/mynewsapp/presentation/navgraph/NavGraph.kt
@@ -0,0 +1,30 @@
+package com.msiprime.mynewsapp.presentation.navgraph
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import com.msiprime.mynewsapp.presentation.onbording.OnBoardingScreen
+import com.msiprime.mynewsapp.presentation.onbording.OnBoardingViewModel
+
+@Composable
+fun NavGraph(
+    startDestination: String
+) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = startDestination) {
+        navigation(
+            route = Route.AppStartNavigation.route,
+            startDestination = Route.OnBoardingScreen.route
+        ) {
+            composable(route = Route.OnBoardingScreen.route) {
+                val viewModel: OnBoardingViewModel = hiltViewModel()
+                OnBoardingScreen(event = viewModel::onEvent)
+            }
+        }
+    }
+
+}
