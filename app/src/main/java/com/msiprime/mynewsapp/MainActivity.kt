dd548a6c9b37bd141aaf1956f885987bacd32d54
msiprime <msisakib958@gmail.com>
2023-11-28 23:20:42 +0600

    part 9 implemented successfully

--- app/src/main/java/com/msiprime/mynewsapp/MainActivity.kt
+++ app/src/main/java/com/msiprime/mynewsapp/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.msiprime.mynewsapp.presentation.navgraph.NavGraph
 import com.msiprime.mynewsapp.presentation.ui.theme.MyNewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,7 +27,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen().apply {
-            setKeepOnScreenCondition{
+            setKeepOnScreenCondition {
                 viewModel.splashCondition
             }
         }
@@ -39,6 +43,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val isSystemInDarkMode = isSystemInDarkTheme()
+                    val systemController = rememberSystemUiController()
+                    SideEffect {
+                        systemController.setSystemBarsColor(
+                            color = Color.Transparent,
+//                            Color.Blue,
+                            darkIcons = !isSystemInDarkMode
+                        )
+                    }
                     val startDestination = viewModel.startDestination
 //                    val viewModel: OnBoardingViewModel = hiltViewModel()
 //                    OnBoardingScreen(event = viewModel::onEvent)
